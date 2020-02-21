0f1c4a87fa5ec09b7c3f0499bec92212e38a755e
Thiago F. G. Albuquerque <thiagofga@gmail.com>
2020-02-21 14:53:12 -0300

    AssertThrowsMixin

--- test/Test.py
+++ test/Test.py
@@ -3,9 +3,10 @@ from LaunchEditor.launchEditor import getEditor, LaunchEditorException,\
     launchEditor, SystemException
 from mock.mock import patch
 import os
+from AssertThrowsMixin import AssertThrowsMixin
 
 
-class Test(unittest.TestCase):
+class Test(unittest.TestCase, AssertThrowsMixin):
 
     @patch.dict(os.environ, { 'EDITOR': 'my-editor' })
     def test_getEditor_happy_path(self):
@@ -16,22 +17,14 @@ class Test(unittest.TestCase):
     @patch('os.environ', {})
     def test_EDITOR_not_set(self):
         
-        with self.assertRaises(LaunchEditorException) as cm:
-            
-            getEditor()
-            
-        self.assertEqual(cm.exception.message, '$EDITOR undefined')
-
+        self.assertThrows(getEditor, LaunchEditorException, '$EDITOR undefined')
+        
         
     @patch.dict(os.environ, { 'EDITOR': 'non-existent-editor' })
     def test_launchEditor_editor_fails(self):
         
-        with self.assertRaises(SystemException) as cm:
-            
-            launchEditor()
-            
-        self.assertEqual(cm.exception.message, 'Editor failed with status 32512')
-
+        self.assertThrows(launchEditor, SystemException, 'Editor failed with status 32512')
+        
         
     def _test_launchEditor_for_real(self):
         
